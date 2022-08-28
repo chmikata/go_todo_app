9bd11be52c58dd38e177dad9ccbf43bb08bf927e
chmikata <chmikata@gmail.com>
2022-08-29 01:33:37 +0900

    task_test実装

--- store/task_test.go
+++ store/task_test.go
@@ -3,20 +3,26 @@ package store
 import (
 	"context"
 	"reflect"
+	"regexp"
 	"testing"
 
+	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/chmikata/go_todo_app/clock"
 	"github.com/chmikata/go_todo_app/entity"
 	"github.com/chmikata/go_todo_app/testutil"
+	"github.com/jmoiron/sqlx"
 )
 
 func TestRepository_ListTasks(t *testing.T) {
+	t.Parallel()
+
 	ctx := context.Background()
 	tx, err := testutil.OpenDBForTest(t).BeginTxx(ctx, nil)
 	if err != nil {
 		t.Fatal(err)
 	}
 	t.Cleanup(func() { _ = tx.Rollback() })
+
 	type fields struct {
 		Clocker clock.Clocker
 	}
@@ -60,22 +66,23 @@ func TestRepository_ListTasks(t *testing.T) {
 	if _, err := tx.ExecContext(ctx, "delete from todoapp.tasks;"); err != nil {
 		t.Errorf("failed to initialize tasks: %v", err)
 	}
-	wants := tests[0].want
-	rows, err := tx.QueryContext(ctx,
-		`insert into todoapp.tasks (title, stat, created, modified) values
-		($1, $2, $3, $4),($5, $6, $7, $8), ($9, $10, $11, $12) returning id;`,
-		wants[0].Title, wants[0].Stat, wants[0].Created, wants[0].Modified,
-		wants[1].Title, wants[1].Stat, wants[1].Created, wants[1].Modified,
-		wants[2].Title, wants[2].Stat, wants[2].Created, wants[2].Modified,
-	)
-	if err != nil {
-		t.Fatal(err)
-	}
-	for i := 0; rows.Next(); i++ {
-		rows.Scan(&wants[i].ID)
-	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			rows, err := tx.QueryContext(ctx,
+				`insert into todoapp.tasks (title, stat, created, modified) values
+				($1, $2, $3, $4),($5, $6, $7, $8), ($9, $10, $11, $12) returning id;`,
+				tt.want[0].Title, tt.want[0].Stat, tt.want[0].Created, tt.want[0].Modified,
+				tt.want[1].Title, tt.want[1].Stat, tt.want[1].Created, tt.want[1].Modified,
+				tt.want[2].Title, tt.want[2].Stat, tt.want[2].Created, tt.want[2].Modified,
+			)
+			if err != nil {
+				t.Fatal(err)
+			}
+			for i := 0; rows.Next(); i++ {
+				if err := rows.Scan(&tt.want[i].ID); err != nil {
+					t.Fatal(err)
+				}
+			}
 			r := &Repository{
 				Clocker: tt.fields.Clocker,
 			}
@@ -97,9 +104,21 @@ func TestRepository_ListTasks(t *testing.T) {
 }
 
 func TestRepository_AddTask(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background()
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Cleanup(func() { _ = db.Close() })
 	type fields struct {
 		Clocker clock.Clocker
 	}
+	xdb := sqlx.NewDb(db, "postgres")
+	fc := clock.FixedClocker{}
+	var wantID int64 = 20
+
 	type args struct {
 		ctx context.Context
 		db  Execer
@@ -111,10 +130,29 @@ func TestRepository_AddTask(t *testing.T) {
 		args    args
 		wantErr bool
 	}{
-		// TODO: Add test cases.
+		{
+			name:   "test1",
+			fields: fields{Clocker: fc},
+			args: args{
+				ctx: ctx,
+				db:  xdb,
+				t: &entity.Task{
+					Title:    "ok test",
+					Stat:     "todo",
+					Created:  fc.Now(),
+					Modified: fc.Now(),
+				},
+			},
+			wantErr: false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			mock.ExpectQuery(
+				regexp.QuoteMeta(`insert into tasks (title, stat, created, modified)
+				values($1, $2, $3, $4) returning id;`),
+			).WithArgs(tt.args.t.Title, tt.args.t.Stat, tt.args.t.Created, tt.args.t.Modified).
+				WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(wantID))
 			r := &Repository{
 				Clocker: tt.fields.Clocker,
 			}
